dc8550beb4984b5714945e8280bf5df23778232e
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-02 10:15:28 -0500

    User Tim Hesselsweet saved changes to main.py at 03/02/2024, 15:15:27 UTC time.

--- 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
+++ 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
@@ -1,51 +1,47 @@
 from surmount.base_class import Strategy, TargetAllocation
+from surmount.technical_indicators import SMA, BB
 from surmount.logging import log
-from surmount.data import Asset, InstitutionalOwnership
-import pandas_ta as ta
-import pandas as pd
-
-def SMAVol(ticker, data, length):
-    '''Calculate the moving average of trading volume
-
-    :param ticker: a string ticker
-    :param data: data as provided from the OHLCV data function
-    :param length: the window
-
-    :return: list with float SMA
-    '''
-    close = [i[ticker]["volume"] for i in data]
-    d = ta.sma(pd.Series(close), length=length)
-    if d is None:
-        return None
-    return d.tolist()
 
 class TradingStrategy(Strategy):
-    def __init__(self):
-        self.tickers = ["VIRT"]
-        self.data_list = []
-
-    @property
-    def interval(self):
-        return "1day"
 
     @property
     def assets(self):
-        return self.tickers
+        return ["QQQ", "SQQQ"]
 
     @property
-    def data(self):
-        return self.data_list
+    def interval(self):
+        return "1hour"
 
     def run(self, data):
-        vols = [i["VIRT"]["volume"] for i in data["ohlcv"]]
-        smavols = SMAVol("VIRT", data["ohlcv"], 40)
-        smavols2 = SMAVol("VIRT", data["ohlcv"], 10)
+        holdings = data["holdings"]
+        data = data["ohlcv"]
 
-        if len(vols)==0:
-                return TargetAllocation({})
+        sqqq_stake = 0
+        qqq_stake = 0
 
-        if smavols2[-1]/smavols[-1]-1>0:
-                out = smavols2[-1]/smavols[-1]-1
-        else: out = 0
+        qqq_bbands = BB("QQQ", data, 20, 1.4)
+        qqq_ma = SMA("QQQ", data, 5)
 
-        return TargetAllocation({"VIRT": min(0.9, (out*10)**(0.5))})
+        if len(data)<20:
+            return TargetAllocation({})
+
+        current_price = data[-1]["QQQ"]['close']
+
+        if qqq_bbands is not None and current_price < qqq_bbands['lower'][-1] and qqq_ma[-1]>qqq_ma[-2]:
+            log("going long")
+            if holdings["QQQ"] >= 0:
+                qqq_stake = min(1, holdings["QQQ"]+0.1)
+            else:
+                qqq_stake = 0.4
+        elif qqq_bbands is not None and current_price > qqq_bbands['upper'][-1]:
+            log("going short")
+            if holdings["SQQQ"] >= 0:
+                sqqq_stake = min(1, holdings["SQQQ"]+0.075)
+            else:
+                sqqq_stake = 0.2
+        else:
+            log("meh")
+            qqq_stake = 0
+            sqqq_stake = 0
+
+        return TargetAllocation({"SQQQ": sqqq_stake, "QQQ": qqq_stake})
