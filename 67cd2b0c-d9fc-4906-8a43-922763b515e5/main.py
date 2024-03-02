d757f68f227a816ae821cd8b58ff696eba709775
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-02 08:33:41 -0500

    User Tim Hesselsweet saved changes to multiple files files at 03/02/2024, 13:33:40 UTC time.

--- 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
+++ 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
@@ -15,7 +15,7 @@ def SMAVol(ticker, data, length):
     '''
     close = [i[ticker]["volume"] for i in data]
     d = ta.sma(pd.Series(close), length=length)
-    print('d: ', d);
+
     if d is None:
         return None
     return d.tolist()
@@ -38,6 +38,7 @@ class TradingStrategy(Strategy):
         return self.data_list
 
     def run(self, data):
+        print('hello')
         vols = [i["VIRT"]["volume"] for i in data["ohlcv"]]
         smavols = SMAVol("VIRT", data["ohlcv"], 40)
         smavols2 = SMAVol("VIRT", data["ohlcv"], 10)
