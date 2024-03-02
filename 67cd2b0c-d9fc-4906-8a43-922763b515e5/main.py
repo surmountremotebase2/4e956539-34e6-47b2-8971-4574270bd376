8f7b925956addbad919236681e6d2950be2557b0
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-02 10:14:09 -0500

    User Tim Hesselsweet saved changes to main.py at 03/02/2024, 15:14:08 UTC time.

--- 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
+++ 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
@@ -1,27 +1,51 @@
 from surmount.base_class import Strategy, TargetAllocation
-from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB
 from surmount.logging import log
+from surmount.data import Asset, InstitutionalOwnership
+import pandas_ta as ta
+import pandas as pd
+
+def SMAVol(ticker, data, length):
+    '''Calculate the moving average of trading volume
+
+    :param ticker: a string ticker
+    :param data: data as provided from the OHLCV data function
+    :param length: the window
+
+    :return: list with float SMA
+    '''
+    close = [i[ticker]["volume"] for i in data]
+    d = ta.sma(pd.Series(close), length=length)
+    if d is None:
+        return None
+    return d.tolist()
 
 class TradingStrategy(Strategy):
     def __init__(self):
-        self.tickers = ["GOOGL"]
+        self.tickers = ["VIRT"]
+        self.data_list = []
 
-    @property
-    def assets(self):
-        return self.tickers
-        
     @property
     def interval(self):
         return "1day"
 
+    @property
+    def assets(self):
+        return self.tickers
+
+    @property
+    def data(self):
+        return self.data_list
+
     def run(self, data):
-        d = data["ohlcv"]
-        allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
-        log(str(allocation_dict))
-        log(str(d))
-        log(str(len(d)))
+        vols = [i["VIRT"]["volume"] for i in data["ohlcv"]]
+        smavols = SMAVol("VIRT", data["ohlcv"], 40)
+        smavols2 = SMAVol("VIRT", data["ohlcv"], 10)
 
-        allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
+        if len(vols)==0:
+                return TargetAllocation({})
 
-        log(str(allocation_dict))
-        return TargetAllocation(allocation_dict)
+        if smavols2[-1]/smavols[-1]-1>0:
+                out = smavols2[-1]/smavols[-1]-1
+        else: out = 0
+
+        return TargetAllocation({"VIRT": min(0.9, (out*10)**(0.5))})
