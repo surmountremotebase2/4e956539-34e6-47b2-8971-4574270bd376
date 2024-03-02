783650d292cfddc67d049226b7084f2f8a17b445
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-02 09:44:42 -0500

    User Tim Hesselsweet saved changes to main.py at 03/02/2024, 14:44:41 UTC time.

--- 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
+++ 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
@@ -3,11 +3,13 @@ from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB
 from surmount.logging import log
 
 class TradingStrategy(Strategy):
+    def __init__(self):
+        self.tickers = ["SPY", "QQQ", "AAPL", "GOOGL"]
 
     @property
     def assets(self):
-        return ["AAPL", "GOOGL"]
-
+        return self.tickers
+        
     @property
     def interval(self):
         return "1day"
