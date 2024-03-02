5f49ae7f4be922eb9eec2c0266e86b2449396bb7
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-02 09:14:44 -0500

    User Tim Hesselsweet saved changes to main.py at 03/02/2024, 14:14:43 UTC time.

--- 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
+++ 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
@@ -1,26 +1,24 @@
 from surmount.base_class import Strategy, TargetAllocation
 from surmount.technical_indicators import RSI, EMA, SMA, MACD, MFI, BB
 from surmount.logging import log
-from surmount.data import Asset, InstitutionalOwnership, InsiderTrading
 
 class TradingStrategy(Strategy):
 
-    def __init__(self):
-        self.tickers = ["SPY", "QQQ", "AAPL", "GOOGL"]
-        self.data_list = [InstitutionalOwnership(i) for i in self.tickers]
-        self.data_list += [InsiderTrading(i) for i in self.tickers]
+    @property
+    def assets(self):
+        return ["QQQ"]
 
     @property
     def interval(self):
-        return "1day"
-
-    @property
-    def assets(self):
-        return self.tickers
-
-    @property
-    def data(self):
-        return self.data_list
+        return "1hour"
 
     def run(self, data):
-        return TargetAllocation()
+        d = data["ohlcv"]
+        qqq_stake = 0
+        if len(d)>3 and "13:00" in d[-1]["QQQ"]["date"]:
+            v_shape = d[-2]["QQQ"]["close"]<d[-3]["QQQ"]["close"] and d[-1]["QQQ"]["close"]>d[-2]["QQQ"]["close"]
+            log(str(v_shape))
+            if v_shape:
+                qqq_stake = 1
+
+        return TargetAllocation({"QQQ": qqq_stake})
