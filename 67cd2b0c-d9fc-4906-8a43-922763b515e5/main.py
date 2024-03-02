9fde6632747d70dd10a44e312f30f1d14b78a0b3
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-02 10:22:14 -0500

    User Tim Hesselsweet saved changes to multiple files files at 03/02/2024, 15:22:13 UTC time.

--- 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
+++ 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
@@ -6,42 +6,17 @@ class TradingStrategy(Strategy):
 
     @property
     def assets(self):
-        return ["SQQQ"]
+        return ["GOOGL"]
 
     @property
     def interval(self):
-        return "1hour"
+        return "1day"
 
     def run(self, data):
-        holdings = data["holdings"]
-        data = data["ohlcv"]
-
-        sqqq_stake = 0
-        qqq_stake = 0
-
-        qqq_bbands = BB("QQQ", data, 20, 1.4)
-        qqq_ma = SMA("QQQ", data, 5)
-
-        if len(data)<20:
-            return TargetAllocation({})
-
-        current_price = data[-1]["QQQ"]['close']
-
-        if qqq_bbands is not None and current_price < qqq_bbands['lower'][-1] and qqq_ma[-1]>qqq_ma[-2]:
-            log("going long")
-            if holdings["QQQ"] >= 0:
-                qqq_stake = min(1, holdings["QQQ"]+0.1)
-            else:
-                qqq_stake = 0.4
-        elif qqq_bbands is not None and current_price > qqq_bbands['upper'][-1]:
-            log("going short")
-            if holdings["SQQQ"] >= 0:
-                sqqq_stake = min(1, holdings["SQQQ"]+0.075)
-            else:
-                sqqq_stake = 0.2
+        allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
+        if len(self.tickers) % 2 == 0:
+            allocation_dict = {i: 1/len(self.tickers) for i in self.tickers}
         else:
-            log("meh")
-            qqq_stake = 0
-            sqqq_stake = 0
+            allocation_dict = {i: 0 for i in self.tickers}
 
-        return TargetAllocation({"SQQQ": sqqq_stake})
+        return TargetAllocation(allocation_dict)
