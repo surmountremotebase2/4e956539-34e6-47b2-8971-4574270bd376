4483c1e1c86c81baa3a8e7c2e02ebe2fc513724b
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-03 10:53:41 -0500

    User Tim Hesselsweet saved changes to multiple files files at 03/03/2024, 15:53:40 UTC time.

--- 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
+++ 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
@@ -150,4 +150,5 @@ class TradingStrategy(Strategy):
         #     return TargetAllocation(self.prev_allocation_dict)
         # log('allocation')
         # log(str(curr_allocation_dict))
-        return TargetAllocation({'SPY': 1})
+        return TargetAllocation({'XLP': 1})
+        
