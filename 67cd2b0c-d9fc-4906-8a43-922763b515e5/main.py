e4835747be4c2cd857c7470a106f2fc6deb2d238
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-03 07:33:54 -0500

    User Tim Hesselsweet saved changes to multiple files files at 03/03/2024, 12:33:53 UTC time.

--- 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
+++ 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
@@ -15,9 +15,25 @@ class TradingStrategy(Strategy):
     def assets(self):
         return self.tickers
 
+    def get_market_open_dates(start_date, end_date, exchange_name='NYSE'):
+
+        calendar = get_calendar(exchange_name)
+    
+        # Generate the market open dates
+        schedule = calendar.schedule(start_date=start_date, end_date=end_date)
+    
+        return schedule.index.tolist()
+
     def run(self, data):
         d = data["ohlcv"]
-        
+
+        start_date = '2024-01-01'
+        end_date = '2024-12-31'
+        exchange_name = 'NYSE'
+
+        market_open_dates = get_market_open_dates(start_date, end_date, exchange_name)
+
+        log(market_open_dates)
         log(str(d[-1]))
         allocation_dict = {i: 0 for i in self.tickers}
         if len(d) % 2 == 1:  
@@ -27,3 +43,5 @@ class TradingStrategy(Strategy):
             log('sell')
             allocation_dict = {"GOOGL": 0.2}
         return TargetAllocation(allocation_dict)
+
+
