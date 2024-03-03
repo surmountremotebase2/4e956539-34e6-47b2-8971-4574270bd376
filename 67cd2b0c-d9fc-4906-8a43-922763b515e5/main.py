5eb00ac2cdc2e872a26e757fa5e26e1ab3edca63
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-03 06:58:15 -0500

    User Tim Hesselsweet saved changes to multiple files files at 03/03/2024, 11:58:14 UTC time.

--- 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
+++ 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
@@ -18,7 +18,7 @@ class TradingStrategy(Strategy):
     def run(self, data):
         d = data["ohlcv"]
         
-        log(str(d))
+        log(str(type(d)))
         allocation_dict = {i: 0 for i in self.tickers}
         if len(d) % 2 == 1:  
             log('buy') 
