d8f874e33bc4b97658f76b549929712ed05dc48f
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-03 06:53:39 -0500

    User Tim Hesselsweet saved changes to multiple files files at 03/03/2024, 11:53:38 UTC time.

--- 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
+++ 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
@@ -5,7 +5,7 @@ from surmount.logging import log
 class TradingStrategy(Strategy):
 
     def __init__(self):
-        self.tickers = ["XLP"]
+        self.tickers = ["XLP", "XLY", "XLE", "XLK", "XLV", "XLI", "XLC", "XLF", "XLU", "XLB"]
 
     @property
     def interval(self):
