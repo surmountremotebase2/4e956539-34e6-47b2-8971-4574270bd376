7c30a66322858eaafafb21421d2de9f7a95fd44e
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-03 09:51:45 -0500

    User Tim Hesselsweet saved changes to multiple files files at 03/03/2024, 14:51:44 UTC time.

--- 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
+++ 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
@@ -113,13 +113,13 @@ class TradingStrategy(Strategy):
 
         data_df, dates = self.get_data(d)
       
-        log('data df: ')
-        log(str(data_df))
+        # log('data df: ')
+        # log(str(data_df))
 
         returns = self.calculate_return(data_df, days=2)
 
-        log('return: ')
-        log(str(returns))
+        # log('return: ')
+        # log(str(returns))
 
         timestamp = dates[-1]
         
