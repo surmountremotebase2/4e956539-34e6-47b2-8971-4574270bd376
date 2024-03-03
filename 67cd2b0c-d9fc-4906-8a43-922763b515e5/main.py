a32adcf19fc2add8f7333df979a77bc601cea748
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-03 07:54:55 -0500

    User Tim Hesselsweet saved changes to main.py at 03/03/2024, 12:54:55 UTC time.

--- 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
+++ 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
@@ -2,6 +2,7 @@ from surmount.base_class import Strategy, TargetAllocation
 from surmount.technical_indicators import SMA, BB
 from surmount.logging import log
 from pandas_market_calendars import get_calendar
+import pandas as pd
 
 class TradingStrategy(Strategy):
 
