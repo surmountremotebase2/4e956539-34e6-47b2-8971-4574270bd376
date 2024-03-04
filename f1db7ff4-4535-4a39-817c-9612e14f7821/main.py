71837c2b06580a13a04107dea2ceb47ba35da9da
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-04 09:12:07 -0500

    User Tim Hesselsweet saved changes to multiple files files at 03/04/2024, 14:12:06 UTC time.

--- f1db7ff4-4535-4a39-817c-9612e14f7821/main.py
+++ f1db7ff4-4535-4a39-817c-9612e14f7821/main.py
@@ -1,4 +1,4 @@
-#Type code herefrom surmount.base_class import Strategy, TargetAllocation
+from surmount.base_class import Strategy, TargetAllocation
 from surmount.logging import log
 from surmount.data import Asset, InstitutionalOwnership
 import pandas_ta as ta
