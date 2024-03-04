ba3749468ccba843c627167d92f3549412948d78
Surmount Strategies <118992879+surmountremotebase2@users.noreply.github.com>
2024-03-04 09:56:54 -0500

    User Tim Hesselsweet saved changes to multiple files files at 03/04/2024, 14:56:53 UTC time.

--- 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
+++ 67cd2b0c-d9fc-4906-8a43-922763b515e5/main.py
@@ -134,7 +134,7 @@ class TradingStrategy(Strategy):
             # log('data df: ')
             # log(str(data_df))
 
-            returns = self.calculate_return(data_df, days=250)
+            returns = self.calculate_return(data_df, days=126)
 
             returns = returns.drop(columns=['SPY'])
 
